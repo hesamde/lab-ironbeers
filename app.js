db25bf4b677e5b2a36c458e0441f46d0c3d0c97d
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-05-23 18:37:15 +0200

    lab

--- app.js
+++ app.js
@@ -22,4 +22,19 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { data: beersFromApi });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).send('database error');
+    });
+});
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
